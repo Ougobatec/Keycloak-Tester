990e2e1f0701cf4a6c9238cac41dba8180ad0644
Hugo Battaglia <hugo.battaglia@hotmail.fr>
2026-06-22 14:27:12 +0200

    🐛 Refactor Vite config to use environment variables for server port

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    port: Number(process.env.PORT) || 3001,
-  },
-  build: {
-    sourcemap: false,
-  },
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      port: Number(env.PORT) || 3001,
+    },
+    build: {
+      sourcemap: false,
+    },
+    plugins: [react(), tailwindcss()],
+  };
 });
